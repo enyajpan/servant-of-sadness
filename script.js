8b2ef69cb3debea7459a857d9815387dd4cbc6d1
enyajpan <enya_pan@brown.edu>
2025-04-15 21:24:13 -0400

    layout changes

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 var w = $(window).width();
 var h = $(window).height();
-var linkBody = "https://enyajpan.github.io/";
 
 var links = {
   "xxx": {
-    "number": "7838975506",
+    "number": "001",
     "subject line": "We sat next to each other on the D train from 36th street to Grand st (New York)",
     "author": "Anonymous",
     "message":"We sat next to each other on the bench at 36th street today around 12:30p. We also sat next to each other on the train, occasionally exchanging glances. You were listening to music and smiling. You're a woman with fair skin, long dark hair parted in the center, and green-blue eyes. I'm a man with curly brown hair, brown eyes, tan skin, and a beard. After you got off at Grand street we locked eyes through the window and both knew it was a missed connection.",
     "timestamp": "2025-03-31 19:20"
-  },
-  "xxx": {
-    "number": "00",
-    "name": "xxx",
-    "author": "xxx",
-    "message":"xxx"
   }
   // Add more entries as needed
 };
@@ -57,23 +50,18 @@ $("#container").on("mouseenter", ".line", function () {
 
 function makeLinks() {
   for (var [key, value] of Object.entries(links)) {
-    let title = value.name;
+    let title = value["subject line"] || "";
     let author = value.author;
     let number = value.number;
     let bio = value.bio;
     let timestamp = value.timestamp;
+    let message = value.message || "";
 
-    let newline = $(`<a class='line' href='${linkBody + key}' target='_top'></a>`);
-    newline.append($(`<div class="column number">RM–${number}</div>`));
+    let newline = $(`<div class='line'></div>`);
+    newline.append($(`<div class="column number">${number}</div>`));
     newline.append($(`<div class="column title">${title}</div>`));
-    newline.append($(`<div class="column by">by</div>`));
-    let authorDiv = $(`<div class="column author">${author}</div>`);
-    if (bio) {
-      let bioDiv = $(`<div class="bio">${bio}</div>`);
-      authorDiv.append(bioDiv);
-    }
-    newline.append(authorDiv);
-    newline.append($(`<div class="column message">undefined</div>`)); // placeholder
+    newline.append($(`<div class="column author">${author}</div>`));
+    newline.append($(`<div class="column message">${message}</div>`));
     newline.append($(`<div class="column timestamp">${timestamp}</div>`));
 
     $("#container").append(newline);
